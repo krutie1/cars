5e983a7f233292fe327e7fce4b311ac3b854f52c
Yeldar <cloderno@gmail.com>
2024-05-10 11:44:18 +0500

    HUGE COMMITS

--- config/app.php
+++ config/app.php
@@ -169,7 +169,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Barryvdh\Debugbar\ServiceProvider::class,
+//        Barryvdh\Debugbar\ServiceProvider::class,
     ])->toArray(),
 
     /*
